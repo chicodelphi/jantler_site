6ff59b949d474ccb6e3427b11180214ca8502dbc
Francisco Fiedler de Vargas Lunardi <93227820+chicodelphi@users.noreply.github.com>
2026-01-08 10:24:12 -0300

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['Playfair Display', 'serif'],
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
